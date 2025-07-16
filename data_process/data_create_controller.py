c7df500c28a59d33ca32a9613808815569c3ceda
Mineo1405 <nhatminh14052004@gmail.com>
2025-07-16 09:55:18 +0700

    Created matchzoo datapack script and train and save model successfully. Also test dml model with pytorch but crash during training phase.

--- data_process/data_create_controller.py
+++ data_process/data_create_controller.py
@@ -1,6 +1,11 @@
 import subprocess
 import os
 import sys
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+    
 import json
 import pandas as pd
 import numpy as np
